bf7ebe4a4ccae0cb690adf903dfa23c153c0930c
Vladimir <gglobalink@gmail.com>
2020-09-25 19:17:58 +0300

    add auth token to console

--- diptest/src/app/auth/auth.component.ts
+++ diptest/src/app/auth/auth.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../shared/services/auth.service';
 
 @Component({
   selector: 'app-auth',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authservice:AuthService) { }
 
-  ngOnInit(): void {
+  async ngOnInit() {
+    var body = {
+      login: "tolstikov",
+      password: "root"
+    }
+   let key = await this.authservice.auth(body);
+   console.log(key);
+    
   }
 
 }
--- diptest/src/app/shared/services/auth.service.ts
+++ diptest/src/app/shared/services/auth.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  private baseUrl = "http://dashboard.kholodov.xyz/api/auth";
+
+  public auth(authinfo){
+    console.log(authinfo);
+    return this.http.post(this.baseUrl,authinfo).toPromise();
+  }
 }
