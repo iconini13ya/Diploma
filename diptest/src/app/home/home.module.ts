ef817674e49aa60b655f77ba4e732ce74bc96dd7
Vladimir <gglobalink@gmail.com>
2020-10-10 17:27:33 +0300

    Add shared module

--- diptest/src/app/home/home.module.ts
+++ diptest/src/app/home/home.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomepageComponent } from './homepage/homepage.component';
+import { SharedModule } from '../shared/sharedmodule/shared.module';
 
 
 @NgModule({
   declarations: [HomepageComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    SharedModule
   ]
 })
 export class HomeModule { }
