25cc68854003beba5112035d6abfa4f41c8d897b
Vladimir <gglobalink@gmail.com>
2020-10-21 15:57:12 +0300

    123

--- diptest/src/app/shared/sharedmodule/shared.module.ts
+++ diptest/src/app/shared/sharedmodule/shared.module.ts
@@ -5,18 +5,17 @@ import { SharedRoutingModule } from './shared-routing.module';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
 
 
 @NgModule({
-  declarations: [SidebarComponent, HeaderComponent, FooterComponent],
+  declarations: [SidebarComponent, HeaderComponent, FooterComponent, MainComponent],
   imports: [
     CommonModule,
     SharedRoutingModule
   ],
   exports:[
-    SidebarComponent,
-    HeaderComponent,
-    FooterComponent
+    MainComponent
   ]
 })
 export class SharedModule { }
