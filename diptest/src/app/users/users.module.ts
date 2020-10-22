7e5ae033b7bafa0e59f284ca8c21877ad965a18a
Vladimir <gglobalink@gmail.com>
2020-10-22 18:34:20 +0300

    Вопрос о верстке

--- diptest/src/app/users/users.module.ts
+++ diptest/src/app/users/users.module.ts
@@ -5,13 +5,15 @@ import { UsersRoutingModule } from './users-routing.module';
 import { UserListComponent } from './user-list/user-list.component';
 import { UserItemComponent } from './user-item/user-item.component';
 import { UsersComponent } from './users.component';
+import { SharedModule } from '../shared/sharedmodule/shared.module';
 
 
 @NgModule({
   declarations: [UserListComponent, UserItemComponent, UsersComponent],
   imports: [
     CommonModule,
-    UsersRoutingModule
+    UsersRoutingModule,
+    SharedModule
   ]
 })
 export class UsersModule { }
